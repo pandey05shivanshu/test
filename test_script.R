5e24d3a123064537bc1f8c8685c3620094e9e1e3
pandey05shivanshu <shivanshu.p23@fms.edu>
2021-11-21 11:11:23 +0530

    initial stages of learning

---
+++ test_script.R
@@ -0,0 +1,18 @@
+v1 <- c(1, 2, 3)
+v2 <- c(TRUE, FALSE, TRUE)
+
+num1 <- c(1, 2, 3, 4, 5)
+num2 <- c(10:20)
+length(num1)
+
+?c()
+?length()
+
+## Data set with NA values
+null_vector <- c(NA, 6, 7, 8, 9, NA, NA, 10)
+
+length(null_vector)
+?is.na
+is.na(null_vector)
+help(is.na)
+
